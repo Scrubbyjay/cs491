0923eae8dff338a160ee92b83f85e5ab6ac6738d
Rafael Courtney McLeary <Rafael@MacBook-Air.local>
2021-10-02 16:49:38 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { StyleSheet, Text, View, Button} from "react-native"
+import {NavigationContainer} from "@react-navigation/native"
+import { createStackNavigator} from "@react-navigation/stack"
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="Go to Details"
+      onPress={() => navigation.navigate('Details')}/>
+    </View>
+  )
+}
+
+function DetailScreen() {
+  return (
+    <View>
+      <Text>Detail Screen</Text>
+    </View>
+  )
+}
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
---
+++ ios/Pods/Target Support Files/Pods-cs491-cs491Tests/Pods-cs491-cs491Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cs491_cs491Tests : NSObject
+@end
+@implementation PodsDummy_Pods_cs491_cs491Tests
+@end
