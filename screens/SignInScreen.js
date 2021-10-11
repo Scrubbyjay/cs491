10291f5af4e26994e4f5f94c1e8438a2819b24a1
Rafael Courtney McLeary <Rafael@MacBook-Air.local>
2021-10-10 21:37:58 -0400

    minor background changes

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -7,15 +7,15 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const SignInScreen = ({navigation}) => {
     return (
         //Container
-        <ScrollView style={{flex: 1, backgroundColor: "#cc0033"}} showsVerticalScrollIndicator={false}>
+        <ScrollView style={{flex: 1, backgroundColor: "#ffff"}} showsVerticalScrollIndicator={false}>
             {/* Brand View */}
             <ImageBackground 
                 source={require('../assets/thumbs.jpg')}
                 style={{
-                    height: Dimensions.get('window').height / 2, resizeMode: 'contain', width: '100%', flex: 1,
+                    height: Dimensions.get('window').height / 2.5, resizeMode: 'contain', width: '100%', flex: 1,
                     }}>
                         <View style={styles.signInView}>
-                            <FontAwesome name='th-large' style={{color: '#red',fontSize:100}}/>
+                            <FontAwesome name='heart' style={{color: '#cc0033',fontSize:100}}/>
                             <Text style={styles.brandViewText}>Rutgers University</Text>
                         </View>
                     </ImageBackground>
@@ -32,8 +32,8 @@ const styles=StyleSheet.create({
         alignItems: 'center',
     },
     brandViewText:{
-        color: '#ffff',
-        fontSize: 40,
+        color: '#cc0033',
+        fontSize: 30,
         fontWeight: 'bold',
         textTransform: 'uppercase',
     }
