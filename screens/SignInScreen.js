fc9b4020cc4f7c3a5b7bb4a5684df626d62b9996
Rafael Courtney McLeary <Rafael@MacBook-Air.local>
2021-10-10 20:12:22 -0400

    added plucky background

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, ScrollView, ImageBackground, Dimensions} from 'react-native';
+import { Text, View, ScrollView, ImageBackground, Dimensions, StyleSheet} from 'react-native';
 import { Icon } from 'react-native-vector-icons/Icon';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -7,16 +7,16 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const SignInScreen = ({navigation}) => {
     return (
         //Container
-        <ScrollView style={{flex: 1, backgroundColor: "#008080"}} showsVerticalScrollIndicator={false}>
+        <ScrollView style={{flex: 1, backgroundColor: "#cc0033"}} showsVerticalScrollIndicator={false}>
             {/* Brand View */}
             <ImageBackground 
-                source={require('../assets/logo.png')}
+                source={require('../assets/thumbs.jpg')}
                 style={{
-                    height: Dimensions.get('window').height / 2.5, 
+                    height: Dimensions.get('window').height / 2, resizeMode: 'contain', width: '100%', flex: 1,
                     }}>
-                        <View>
-                            <FontAwesome name='th-large' style={{fontSize:100}}/>
-                            <Text>Rutgers University</Text>
+                        <View style={styles.signInView}>
+                            <FontAwesome name='th-large' style={{color: '#red',fontSize:100}}/>
+                            <Text style={styles.brandViewText}>Rutgers University</Text>
                         </View>
                     </ImageBackground>
         </ScrollView>
@@ -24,3 +24,17 @@ const SignInScreen = ({navigation}) => {
 };
 
 export default SignInScreen;
+
+const styles=StyleSheet.create({
+    signInView:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    brandViewText:{
+        color: '#ffff',
+        fontSize: 40,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+    }
+})
