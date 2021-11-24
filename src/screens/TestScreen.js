a86542a97852c63bd9443e32b6b39420a2c59d01
Rafael Courtney McLeary <Rafael@MacBook-Air.fios-router.home>
2021-11-24 15:10:50 -0500

    sensors

---
+++ src/screens/TestScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import CustomButton from '../components/CustomButton/CustomButton';
+
+export default function TestScreen() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
